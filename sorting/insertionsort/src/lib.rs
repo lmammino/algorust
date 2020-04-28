e2097e64349985d5cf2810c15c8c20b625ae4ada
Luciano Mammino <lucianomammino@gmail.com>
2020-04-28 11:21:59 +0100

    implemented insertionsort

--- sorting/insertionsort/src/lib.rs
+++ sorting/insertionsort/src/lib.rs
@@ -1,5 +1,13 @@
-pub fn sort<T: PartialOrd>(&mut [T]: coll) {
-    
+pub fn sort<T>(values: &mut [T]) where T: Ord {
+    for i in 1..values.len() {
+        for j in (1..=i).rev() {
+            if values[j] >= values[j-1] {
+                break;
+            }
+
+            values.swap(j, j-1);
+        }
+    }
 }
 
 
@@ -8,7 +16,14 @@ mod tests {
     #[test]
     fn sorts_integers() {
         let mut integers : [i32; 5] = [2,5,1,3,4];
-        super::sort(integers);
+        super::sort(&mut integers);
         assert_eq!(integers, [1,2,3,4,5]);
+    }
+
+    #[test]
+    fn sorts_strings() {
+        let mut strings : [&str; 5] = ["hey","how","are","you","today"];
+        super::sort(&mut strings);
+        assert_eq!(strings, ["are", "hey", "how", "today", "you"]);
     }
 }
