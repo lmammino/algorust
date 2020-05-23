6ff2ea3df3c7f58842f8769ea2ac70363b99ad7a
Luciano <lucianomammino@gmail.com>
2020-05-23 17:07:03 +0100

    README updates and cargo fmt

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# algorust
+# algorust 🎲🦀
+
+![.github/workflows/rust.yaml](https://github.com/lmammino/algorust/workflows/.github/workflows/rust.yaml/badge.svg)
 
 Randomly playing with algorithms and Rust as I read through [The Algorithm Deisgn Manual by S. Skiena](https://www.amazon.co.uk/Algorithm-Design-Manual-Steven-Skiena/dp/1849967202)
+
+## Contributing
+
+Everyone is very welcome to contribute to this project.
+You can contribute just by submitting bugs or suggesting improvements by
+[opening an issue on GitHub](https://github.com/lmammino/algorust/issues).
+
+
+## License
+
+Licensed under [MIT License](LICENSE). © Luciano Mammino.
--- sorting/insertionsort/src/lib.rs
+++ sorting/insertionsort/src/lib.rs
@@ -1,36 +1,43 @@
-pub fn sort<T>(values: &mut [T]) where T: Ord {
+pub fn sort<T>(values: &mut [T])
+where
+    T: Ord,
+{
     for i in 1..values.len() {
         for j in (1..=i).rev() {
-            if values[j] >= values[j-1] {
+            if values[j] >= values[j - 1] {
                 break;
             }
 
-            values.swap(j, j-1);
+            values.swap(j, j - 1);
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn sorts_integers() {
-        let mut integers : [i32; 5] = [2,5,1,3,4];
+        let mut integers: [i32; 5] = [2, 5, 1, 3, 4];
         super::sort(&mut integers);
-        assert_eq!(integers, [1,2,3,4,5]);
+        assert_eq!(integers, [1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn sorts_strings() {
-        let mut strings : [&str; 5] = ["hey","how","are","you","today"];
+        let mut strings: [&str; 5] = ["hey", "how", "are", "you", "today"];
         super::sort(&mut strings);
         assert_eq!(strings, ["are", "hey", "how", "today", "you"]);
     }
 
     #[test]
     fn sorts_chars() {
-        let mut chars : [char; 13] = ['I', 'N', 'S', 'E', 'R', 'T', 'I', 'O', 'N', 'S', 'O', 'R', 'T'];
+        let mut chars: [char; 13] = [
+            'I', 'N', 'S', 'E', 'R', 'T', 'I', 'O', 'N', 'S', 'O', 'R', 'T',
+        ];
         super::sort(&mut chars);
-        assert_eq!(chars, ['E', 'I', 'I', 'N', 'N', 'O', 'O', 'R', 'R', 'S', 'S', 'T', 'T']);
+        assert_eq!(
+            chars,
+            ['E', 'I', 'I', 'N', 'N', 'O', 'O', 'R', 'R', 'S', 'S', 'T', 'T']
+        );
     }
 }
